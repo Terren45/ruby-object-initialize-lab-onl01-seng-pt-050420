54d6b0d59e27eda9c66f83aadc834d499d38cc95
Terren Culberson <culbersonterren@yahoo.com>
2020-05-21 02:51:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,9 @@
 class Dog 
   
-  def initialize(name, breed)
+ def initialize(name, breed)
     @name = name
     @breed = breed
-  end
-  
-  def breed
-    @breed = breed
-    puts "Mutt."
+    
   end
 end
   
